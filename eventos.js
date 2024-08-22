14d9e06a6ff96eedda2740983dc9b713f9fcf3d1
Romina <romina-bustelo@hotmail.com>
2024-08-22 19:31:35 -0300

    Detención de la propagación del evento

--- eventos.js
+++ eventos.js
@@ -2,5 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const div = document.querySelector("div");  
     div.addEventListener("click", function() {  
         alert("Hola! Soy el div");  
-    });  
+    }); 
+    
+    const button = document.querySelector("button");  
+    button.addEventListener("click", function(event) {  
+        event.stopPropagation(); // Detiene la propagación del evento  
+        alert("Hola!");  
+    });
 });
