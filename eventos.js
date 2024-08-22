ef57d423c71b21d667d67afd4da4bf2f59b79257
Romina <romina-bustelo@hotmail.com>
2024-08-22 19:25:49 -0300

    Creación de JS y modificación del HTML

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {  
+    const div = document.querySelector("div");  
+    div.addEventListener("click", function() {  
+        alert("Hola! Soy el div");  
+    });  
+});
